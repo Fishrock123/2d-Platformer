cd5833f4e04a0f95c5a5e01845a1e57ec4a432d8
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-01-06 20:03:57 -0500

    reload scene on player exit boundary

--- Assets/Scripts/PlayerBoundary.cs
+++ Assets/Scripts/PlayerBoundary.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBoundary : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D other) {
 		// Reset player position
 		if (other.tag == "Player") {
-			other.GetComponent<Rigidbody2D>().position = new Vector3(0f, 0f, 0f);
+			// other.GetComponent<Rigidbody2D>().position = new Vector3(0f, 0f, 0f);
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 			return;
 		}
 
