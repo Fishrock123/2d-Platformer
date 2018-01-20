438ea4b3b1136be665e6ccf6ec4f6fff87db74ac
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2018-01-20 17:02:42 -0500

    Prevent collectibles from scoring twice

--- Assets/Scripts/CollectableController.cs
+++ Assets/Scripts/CollectableController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CollectableController : MonoBehaviour {
 
 	public GameController gameController;
+	private bool collected = false;
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.tag == "Player") {
+		if (other.tag == "Player" && !collected) {
+			collected = true;
 			gameController.AddScore(1);
 			Destroy(gameObject);
 		}
